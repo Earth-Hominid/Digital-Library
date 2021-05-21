db3adb4f7f985cf87e7fe4b9b4c8ff4aa47c4308
C <earthhominid@gmail.com>
2021-05-21 09:43:18 -0300

    added src folder to hold index.js and data.js

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import data from "./data.js";
+
+
+
+
+// Create a function that iterates through the library and displays the book information on the page. Book are to be displayed on a new card. 
+const displayLibrary = () => {
+  for(let element of library) {
+    return {};
+}
+}
+
+
+displayLibrary();
+
+
+deletButton.addEventListener('click',  )
+
+
+// Create a function to add a book to the library.
