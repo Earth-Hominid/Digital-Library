b8dae1206581ad193646ed67c69e227b8dd1f498
C <earthhominid@gmail.com>
2021-06-28 12:50:09 -0300

    book display updated and logic added

--- src/index.js
+++ src/index.js
@@ -1,64 +1,59 @@
-const myLibrary = [
+function Book(title, author, pages, read, cover) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.cover = cover;
+}
+
+let myLibrary = [
   {
-    title: "JavaScript: The Good Parts", 
-    author: "by Douglas Crockford", 
-    pages: 172, 
-    pagesRead: 0, 
-    cover: "images/good.jpeg",
+    title: 'JavaScript: The Good Parts',
+    author: 'by Douglas Crockford',
+    pages: 172,
+    cover: 'images/good.jpeg',
+    read: false,
   },
   {
-    title: "JavaScript: The Definitive Guide", 
-    author: "by David Flanagan", 
-    pages: 706, 
-    pagesRead: 200,
-   cover: "images/def.jpg", 
+    title: 'JavaScript: The Definitive Guide',
+    author: 'by David Flanagan',
+    pages: 706,
+    cover: 'images/def.jpg',
+    read: true,
   },
   {
-    title: "Eloquent JavaScript: A Modern Introduction to Programming", 
-    author: "by Marijn Haverbeke", 
-    pages: 472, pagesRead: 0,
-   cover: "images/elo.jpeg",
-  }
+    title: 'Eloquent JavaScript: A Modern Introduction to Programming',
+    author: 'by Marijn Haverbeke',
+    pages: 472,
+    cover: 'images/elo.jpeg',
+    read: true,
+  },
 ];
 
 const displayBook = (book) => {
   // Create HTML elements to display the book on a card
   const cardDiv = document.createElement('div');
-  cardDiv.classList.add("cardDiv")
-  const displayBookTitle = document.createElement('h3');
-  const displayBookAuthor = document.createElement('h4');
-  const displayBookPages = document.createElement('h5');
-  const displayBookPagesRead = document.createElement('h5');
+  const bookTitle = document.createElement('h3');
+  const bookAuthor = document.createElement('h4');
+  const bookPages = document.createElement('h5');
+  const bookRead = document.createElement('h5');
   const displayCover = document.createElement('img');
-
   // select body element for newly created elements to be appended
   const show = document.querySelector('div.book');
-  
   // Append the elements to the DOM
-  show.appendChild(cardDiv); 
-  cardDiv.append(displayCover, displayBookTitle, displayBookAuthor, displayBookPages, displayBookPagesRead);
-  
+  cardDiv.classList.add('cardDiv');
+  show.appendChild(cardDiv);
+  cardDiv.append(displayCover, bookTitle, bookAuthor, bookPages, bookRead);
   // Set content and attributes
-  displayBookTitle.innerHTML = book.title;
-  displayBookAuthor.innerHTML = book.author;
-  displayBookPages.innerHTML = "Pages: " + book.pages;
-  displayBookPagesRead.innerHTML = "Pages Read: " + book.pagesRead;
-  // use setAttribute to add the card class to the div
-
-  // cardDiv.setAttribute("class", "card");
+  bookTitle.textContent = book.title;
+  bookAuthor.textContent = book.author;
+  bookPages.textContent = 'Pages: ' + book.pages;
+  bookRead.textContent = book.read ? 'Finished Reading' : 'Not finished';
   displayCover.setAttribute('src', book.cover);
-  displayCover.setAttribute("id", "placeholder")
+  displayCover.setAttribute('id', 'placeholder');
 };
 
-myLibrary.forEach(book => displayBook(book))
-
-
-
-
-
-
-
-
+myLibrary.forEach((book) => displayBook(book));
 
 
 
