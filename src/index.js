f69d5830af8b602546edb7aacc5f9fef988372cb
C <earthhominid@gmail.com>
2021-07-13 14:32:35 -0300

    changed back to single quotes

--- src/index.js
+++ src/index.js
@@ -10,91 +10,91 @@ class Book {
 
 let myLibrary = [
   {
-    title: "JavaScript: The Good Parts",
-    author: "by Douglas Crockford",
+    title: 'JavaScript: The Good Parts',
+    author: 'by Douglas Crockford',
     pages: 172,
-    cover: "images/good.jpeg",
+    cover: 'images/good.jpeg',
     read: false,
   },
   {
-    title: "JavaScript: The Definitive Guide",
-    author: "by David Flanagan",
+    title: 'JavaScript: The Definitive Guide',
+    author: 'by David Flanagan',
     pages: 706,
-    cover: "images/def.jpg",
+    cover: 'images/def.jpg',
     read: true,
   },
   {
-    title: "Eloquent JavaScript: A Modern Introduction to Programming",
-    author: "by Marijn Haverbeke",
+    title: 'Eloquent JavaScript: A Modern Introduction to Programming',
+    author: 'by Marijn Haverbeke',
     pages: 472,
-    cover: "images/elo.jpeg",
+    cover: 'images/elo.jpeg',
     read: true,
   },
 ];
 
 const displayBook = (book) => {
   // Create HTML elements to display the book on a card
-  const cardDiv = document.createElement("div");
-  const bookTitle = document.createElement("h3");
-  const bookAuthor = document.createElement("h4");
-  const bookPages = document.createElement("h5");
-  const bookRead = document.createElement("h5");
-  const displayCover = document.createElement("img");
+  const cardDiv = document.createElement('div');
+  const bookTitle = document.createElement('h3');
+  const bookAuthor = document.createElement('h4');
+  const bookPages = document.createElement('h5');
+  const bookRead = document.createElement('h5');
+  const displayCover = document.createElement('img');
   // select body element for newly created elements to be appended
-  const show = document.querySelector("div.book");
+  const show = document.querySelector('div.book');
   // Append the elements to the DOM
-  cardDiv.classList.add("cardDiv");
+  cardDiv.classList.add('cardDiv');
   show.appendChild(cardDiv);
   cardDiv.append(displayCover, bookTitle, bookAuthor, bookPages, bookRead);
   // Set content and attributes
   bookTitle.textContent = book.title;
   bookAuthor.textContent = book.author;
-  bookPages.textContent = "Pages: " + book.pages;
-  bookRead.textContent = book.read ? "Finished Reading" : "Not finished";
-  displayCover.setAttribute("src", book.cover);
-  displayCover.setAttribute("id", "placeholder");
+  bookPages.textContent = 'Pages: ' + book.pages;
+  bookRead.textContent = book.read ? 'Finished Reading' : 'Not finished';
+  displayCover.setAttribute('src', book.cover);
+  displayCover.setAttribute('id', 'placeholder');
 };
 
 myLibrary.forEach((book) => displayBook(book));
 
 const showLibrary = () => {
-  document.getElementById("my-library").style.display = "block";
-  document.getElementById("library").scrollIntoView();
+  document.getElementById('my-library').style.display = 'block';
+  document.getElementById('library').scrollIntoView();
 };
 
 const showForm = () => {
-  document.getElementById("form-container").style.display = "block";
-  document.getElementById("form-container").scrollIntoView();
+  document.getElementById('form-container').style.display = 'block';
+  document.getElementById('form-container').scrollIntoView();
 };
 
 const addButton = () => {
-  let buttonContainer = document.querySelector(".button-container");
+  let buttonContainer = document.querySelector('.button-container');
   // Create <div> element
-  const buttonDiv = document.createElement("div");
-  buttonDiv.id = "buttonArea"; // Set its id to "buttonArea"
+  const buttonDiv = document.createElement('div');
+  buttonDiv.id = 'buttonArea'; // Set its id to "buttonArea"
   buttonContainer.appendChild(buttonDiv); // Add <div> element to the DOM
   // Add <button> element
-  const addBookButton = document.createElement("button");
+  const addBookButton = document.createElement('button');
   // Set <button> id and add text
-  addBookButton.id = "addButton";
-  addBookButton.textContent = "I want to add a book";
+  addBookButton.id = 'addButton';
+  addBookButton.textContent = 'I want to add a book';
   // Add <button> element to the created <div> element
-  const addButton = document.getElementById("buttonArea");
+  const addButton = document.getElementById('buttonArea');
   addButton.appendChild(addBookButton);
   // Add an event listener to “addButton” that when clicked, the “addBookToLibrary” function runs.
-  document.getElementById("addButton").addEventListener("click", showForm);
+  document.getElementById('addButton').addEventListener('click', showForm);
 };
 
 addButton();
 
 const removeButton = () => {
   // Add <button> element
-  const removeBookButton = document.createElement("button");
+  const removeBookButton = document.createElement('button');
   // Set <button> id and add text
-  removeBookButton.id = "remove";
-  removeBookButton.textContent = "I want to remove a book";
+  removeBookButton.id = 'remove';
+  removeBookButton.textContent = 'I want to remove a book';
   // Add <button> element to the created <div> element
-  const addButton = document.getElementById("buttonArea");
+  const addButton = document.getElementById('buttonArea');
   addButton.appendChild(removeBookButton);
   // Add an event listener to “addButton” that when clicked, the “addBookToLibrary” function runs.
 };
@@ -102,228 +102,228 @@ const removeButton = () => {
 removeButton();
 
 const createForm = () => {
-  const formContainer = document.createElement("div");
-  formContainer.id = "form-container";
-  formContainer.setAttribute("style", "display:none");
+  const formContainer = document.createElement('div');
+  formContainer.id = 'form-container';
+  formContainer.setAttribute('style', 'display:none');
   document.body.append(formContainer);
 
-  const formTitle = document.createElement("div");
-  formTitle.classList.add("title");
-  formTitle.textContent = "Enter your book details";
+  const formTitle = document.createElement('div');
+  formTitle.classList.add('title');
+  formTitle.textContent = 'Enter your book details';
   formContainer.append(formTitle);
 
-  const content = document.createElement("div");
-  content.classList.add("content");
+  const content = document.createElement('div');
+  content.classList.add('content');
   formContainer.append(content);
 
-  const form = document.createElement("form");
-  form.id = "addbookform";
+  const form = document.createElement('form');
+  form.id = 'addbookform';
   content.append(form);
 
-  const bookDetails = document.createElement("div");
-  bookDetails.classList.add("book-details");
+  const bookDetails = document.createElement('div');
+  bookDetails.classList.add('book-details');
   form.append(bookDetails);
 
   // Book Title
-  const titleDiv = document.createElement("div");
-  titleDiv.classList.add("input-box");
+  const titleDiv = document.createElement('div');
+  titleDiv.classList.add('input-box');
   bookDetails.append(titleDiv);
 
-  const titleLabel = document.createElement("label");
-  titleLabel.setAttribute("for", "title");
-  titleLabel.textContent = "Title";
+  const titleLabel = document.createElement('label');
+  titleLabel.setAttribute('for', 'title');
+  titleLabel.textContent = 'Title';
   titleDiv.append(titleLabel);
 
-  const titleInput = document.createElement("input");
-  titleInput.id = "title";
-  titleInput.setAttribute("type", "text");
-  titleInput.setAttribute("name", "title");
-  titleInput.setAttribute("placeholder", "Enter the title");
-  titleInput.setAttribute("autocomplete", "off");
+  const titleInput = document.createElement('input');
+  titleInput.id = 'title';
+  titleInput.setAttribute('type', 'text');
+  titleInput.setAttribute('name', 'title');
+  titleInput.setAttribute('placeholder', 'Enter the title');
+  titleInput.setAttribute('autocomplete', 'off');
   titleInput.required = true;
   titleDiv.append(titleInput);
 
   // Book Author
-  const authorDiv = document.createElement("div");
-  authorDiv.classList.add("input-box");
+  const authorDiv = document.createElement('div');
+  authorDiv.classList.add('input-box');
   bookDetails.append(authorDiv);
 
-  const authorLabel = document.createElement("label");
-  authorLabel.setAttribute("for", "author");
-  authorLabel.textContent = "Author";
+  const authorLabel = document.createElement('label');
+  authorLabel.setAttribute('for', 'author');
+  authorLabel.textContent = 'Author';
   authorDiv.append(authorLabel);
 
-  const authorInput = document.createElement("input");
-  authorInput.id = "author";
-  authorInput.setAttribute("type", "text");
-  authorInput.setAttribute("name", "author");
-  authorInput.setAttribute("placeholder", "Enter the author");
-  authorInput.setAttribute("autocomplete", "off");
+  const authorInput = document.createElement('input');
+  authorInput.id = 'author';
+  authorInput.setAttribute('type', 'text');
+  authorInput.setAttribute('name', 'author');
+  authorInput.setAttribute('placeholder', 'Enter the author');
+  authorInput.setAttribute('autocomplete', 'off');
   authorInput.required = true;
   authorDiv.append(authorInput);
 
   // Page Number Total
-  const pagesDiv = document.createElement("div");
-  pagesDiv.classList.add("input-box");
+  const pagesDiv = document.createElement('div');
+  pagesDiv.classList.add('input-box');
   bookDetails.append(pagesDiv);
 
-  const pagesLabel = document.createElement("label");
-  pagesLabel.setAttribute("for", "pages");
-  pagesLabel.textContent = "Pages";
+  const pagesLabel = document.createElement('label');
+  pagesLabel.setAttribute('for', 'pages');
+  pagesLabel.textContent = 'Pages';
   pagesDiv.append(pagesLabel);
 
-  const pagesInput = document.createElement("input");
-  pagesInput.id = "pages";
-  pagesInput.setAttribute("type", "number");
-  pagesInput.setAttribute("name", "pages");
-  pagesInput.setAttribute("min", "0");
-  pagesInput.setAttribute("max", "9999");
-  pagesInput.setAttribute("placeholder", "Enter the number of pages");
+  const pagesInput = document.createElement('input');
+  pagesInput.id = 'pages';
+  pagesInput.setAttribute('type', 'number');
+  pagesInput.setAttribute('name', 'pages');
+  pagesInput.setAttribute('min', '0');
+  pagesInput.setAttribute('max', '9999');
+  pagesInput.setAttribute('placeholder', 'Enter the number of pages');
   pagesInput.required = true;
   pagesDiv.append(pagesInput);
 
   // Book Cover
-  const coverDiv = document.createElement("div");
-  coverDiv.classList.add("input-box");
+  const coverDiv = document.createElement('div');
+  coverDiv.classList.add('input-box');
   bookDetails.append(coverDiv);
 
-  const coverLabel = document.createElement("label");
-  coverLabel.setAttribute("for", "cover");
-  coverLabel.textContent = "Image URL";
+  const coverLabel = document.createElement('label');
+  coverLabel.setAttribute('for', 'cover');
+  coverLabel.textContent = 'Image URL';
   coverDiv.append(coverLabel);
 
-  const coverInput = document.createElement("input");
-  coverInput.id = "cover";
-  coverInput.setAttribute("type", "url");
-  coverInput.setAttribute("name", "cover");
-  coverInput.setAttribute("placeholder", "www.example.com (not required)");
+  const coverInput = document.createElement('input');
+  coverInput.id = 'cover';
+  coverInput.setAttribute('type', 'url');
+  coverInput.setAttribute('name', 'cover');
+  coverInput.setAttribute('placeholder', 'www.example.com (not required)');
   coverDiv.append(coverInput);
 
   // Has book been finished checkboxes
-  const readDiv = document.createElement("div");
-  readDiv.classList.add("read-details");
+  const readDiv = document.createElement('div');
+  readDiv.classList.add('read-details');
   form.append(readDiv);
 
-  const inputRead = document.createElement("input");
-  inputRead.setAttribute("type", "radio");
-  inputRead.setAttribute("name", "read");
-  inputRead.id = "dot-1";
+  const inputRead = document.createElement('input');
+  inputRead.setAttribute('type', 'radio');
+  inputRead.setAttribute('name', 'read');
+  inputRead.id = 'dot-1';
   readDiv.append(inputRead);
 
-  const secondInputRead = document.createElement("input");
-  secondInputRead.setAttribute("type", "radio");
-  secondInputRead.setAttribute("name", "read");
-  secondInputRead.id = "dot-2";
+  const secondInputRead = document.createElement('input');
+  secondInputRead.setAttribute('type', 'radio');
+  secondInputRead.setAttribute('name', 'read');
+  secondInputRead.id = 'dot-2';
   readDiv.append(secondInputRead);
 
-  const readSpanTitle = document.createElement("span");
-  readSpanTitle.classList.add("read-title");
-  readSpanTitle.textContent = "Finished Reading?";
+  const readSpanTitle = document.createElement('span');
+  readSpanTitle.classList.add('read-title');
+  readSpanTitle.textContent = 'Finished Reading?';
   readDiv.append(readSpanTitle);
 
-  const readOrNot = document.createElement("div");
-  readOrNot.classList.add("category");
+  const readOrNot = document.createElement('div');
+  readOrNot.classList.add('category');
   readDiv.append(readOrNot);
 
-  const readLabel = document.createElement("label");
-  readLabel.setAttribute("for", "dot-1");
+  const readLabel = document.createElement('label');
+  readLabel.setAttribute('for', 'dot-1');
   readOrNot.append(readLabel);
 
-  const spanDotOne = document.createElement("span");
-  spanDotOne.classList.add("dot");
-  spanDotOne.id = "one";
+  const spanDotOne = document.createElement('span');
+  spanDotOne.classList.add('dot');
+  spanDotOne.id = 'one';
   readLabel.appendChild(spanDotOne);
 
-  const secondSpanDotOne = document.createElement("span");
-  secondSpanDotOne.classList.add("yes-no");
-  secondSpanDotOne.textContent = "Yes";
+  const secondSpanDotOne = document.createElement('span');
+  secondSpanDotOne.classList.add('yes-no');
+  secondSpanDotOne.textContent = 'Yes';
   readLabel.append(secondSpanDotOne);
 
-  const secondReadLabel = document.createElement("label");
-  secondReadLabel.setAttribute("for", "dot-2");
+  const secondReadLabel = document.createElement('label');
+  secondReadLabel.setAttribute('for', 'dot-2');
   readOrNot.append(secondReadLabel);
 
-  const spanDotTwo = document.createElement("span");
-  spanDotTwo.classList.add("dot");
-  spanDotTwo.id = "two";
+  const spanDotTwo = document.createElement('span');
+  spanDotTwo.classList.add('dot');
+  spanDotTwo.id = 'two';
   secondReadLabel.append(spanDotTwo);
 
-  const secondSpanDotTwo = document.createElement("span");
-  secondSpanDotTwo.classList.add("yes-no");
-  secondSpanDotTwo.textContent = "No";
+  const secondSpanDotTwo = document.createElement('span');
+  secondSpanDotTwo.classList.add('yes-no');
+  secondSpanDotTwo.textContent = 'No';
   secondReadLabel.append(secondSpanDotTwo);
 
-  const submitFormButton = document.createElement("div");
-  submitFormButton.classList.add("button");
+  const submitFormButton = document.createElement('div');
+  submitFormButton.classList.add('button');
   form.append(submitFormButton);
 
-  const formButton = document.createElement("input");
-  formButton.setAttribute("type", "submit");
-  formButton.id = "btn";
-  formButton.setAttribute("value", "Submit");
+  const formButton = document.createElement('input');
+  formButton.setAttribute('type', 'submit');
+  formButton.id = 'btn';
+  formButton.setAttribute('value', 'Submit');
 
   submitFormButton.append(formButton);
 };
 
 createForm();
 
-document.getElementById("btn").addEventListener("click", addBookToLibrary);
+document.getElementById('btn').addEventListener('click', addBookToLibrary);
 
 function addBookToLibrary() {
-  let title = document.getElementById("title").value;
-  let author = document.getElementById("author").value;
-  let pages = document.getElementById("pages").value;
-  let cover = document.getElementById("cover").value;
+  let title = document.getElementById('title').value;
+  let author = document.getElementById('author').value;
+  let pages = document.getElementById('pages').value;
+  let cover = document.getElementById('cover').value;
   let read;
 
-  if (document.getElementById("dot-1").checked) {
+  if (document.getElementById('dot-1').checked) {
     read = true;
-  } else if (document.getElementById("dot-2").checked) {
+  } else if (document.getElementById('dot-2').checked) {
     read = false;
   }
 
-  if (!cover) cover = "images/blank-img.png";
+  if (!cover) cover = 'images/blank-img.png';
 
   const book = new Book(title, author, pages, read, cover);
   myLibrary.push(book);
-  console.warn("added", { book });
+  console.warn('added', { book });
   console.log({ book });
-  document.querySelector("form").reset(); // to clear the form for the next entries
+  document.querySelector('form').reset(); // to clear the form for the next entries
   refreshLibrary();
 
   return false;
 }
 
 const refreshLibrary = () => {
-  const library = document.getElementById("library");
-  library.textContent = "";
+  const library = document.getElementById('library');
+  library.textContent = '';
   myLibrary.forEach((book) => {
     // Create HTML elements to display the book on a card
-    const cardDiv = document.createElement("div");
-    const bookTitle = document.createElement("h3");
-    const bookAuthor = document.createElement("h4");
-    const bookPages = document.createElement("h5");
-    const bookRead = document.createElement("h5");
-    const displayCover = document.createElement("img");
+    const cardDiv = document.createElement('div');
+    const bookTitle = document.createElement('h3');
+    const bookAuthor = document.createElement('h4');
+    const bookPages = document.createElement('h5');
+    const bookRead = document.createElement('h5');
+    const displayCover = document.createElement('img');
 
     // select body element for newly created elements to be appended
-    const show = document.querySelector("div.book");
+    const show = document.querySelector('div.book');
 
     // Append the elements to the DOM
-    cardDiv.classList.add("cardDiv");
+    cardDiv.classList.add('cardDiv');
     show.appendChild(cardDiv);
     cardDiv.append(displayCover, bookTitle, bookAuthor, bookPages, bookRead);
 
     // Set content and attributes
     bookTitle.textContent = book.title;
     bookAuthor.textContent = book.author;
-    bookPages.textContent = "Pages: " + book.pages;
-    bookRead.textContent = book.read ? "Finished Reading" : "Not finished";
-    displayCover.setAttribute("src", book.cover);
+    bookPages.textContent = 'Pages: ' + book.pages;
+    bookRead.textContent = book.read ? 'Finished Reading' : 'Not finished';
+    displayCover.setAttribute('src', book.cover);
     displayCover.setAttribute(
-      "onerror",
+      'onerror',
       "this.onerror=null;this.src='images/blank-img.png'"
     );
-    displayCover.setAttribute("id", "placeholder");
+    displayCover.setAttribute('id', 'placeholder');
   });
 };
